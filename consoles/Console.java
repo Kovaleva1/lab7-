420d08d5f99be43f7685d7649f70ecce8bcacb72
Kovaleva1 <115421637+Kovaleva1@users.noreply.github.com>
2023-06-05 01:04:02 +0300

    Add files via upload

---
+++ consoles/Console.java
@@ -0,0 +1,10 @@
+package consoles;
+
+public interface Console {
+
+    public boolean hasNext();
+
+    public String getNextStr();
+
+    public void write(String text);
+}
---
+++ consoles/StringConsole.java
@@ -0,0 +1,24 @@
+package consoles;
+
+public class StringConsole implements Console {
+    private String res = "";
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public String getNextStr() {
+        return null;
+    }
+
+    @Override
+    public void write(String text) {
+        res += text + "\n";
+    }
+
+    public String getAllText() {
+        return res.strip();
+    }
+}
