5baae8bfe1ad1a01dbdd2b58b03b112d3f1f08cc
Kovaleva1 <115421637+Kovaleva1@users.noreply.github.com>
2023-06-08 04:48:45 +0300

    Add files via upload

--- server/Configuration.java
+++ server/Configuration.java
@@ -1,50 +1,9 @@
 package server;
 
-/**
- * Класс конфигураций сервера (порт, хост,
- * название файла со стартовым состоянием коллекции, название файла с историей состояний)
- */
 public class Configuration {
-    private static String host;  // хост, на котором запускается сервер
-    private static int port;  // порт, на котором запускается сервер
-    private static String startFileName;  //файл, содержащий коллекцию
-    private static String historyFileName;  // файл, содержащий состояния коллекции
-
     private static String dbUrl;   // url-адрес для подключения к бд
     private static String dbLogin; // логин для подключения к бд
     private static String dbPass;  //пароль для подключения
-
-    public static String getHost() {
-        return host;
-    }
-
-    public static void setHost(String host) {
-        Configuration.host = host;
-    }
-
-    public static int getPort() {
-        return port;
-    }
-
-    public static void setPort(int port) {
-        Configuration.port = port;
-    }
-
-    public static String getStartFileName() {
-        return startFileName;
-    }
-
-    public static void setStartFileName(String startFileName) {
-        Configuration.startFileName = startFileName;
-    }
-
-    public static String getHistoryFileName() {
-        return historyFileName;
-    }
-
-    public static void setHistoryFileName(String historyFileName) {
-        Configuration.historyFileName = historyFileName;
-    }
 
     public static String getDbUrl() {
         return dbUrl;
--- server/ServerUser.java
+++ server/ServerUser.java
@@ -1,30 +1,22 @@
 package server;
 
-import models.UserRole;
-
 public class ServerUser {
         private final int id;
         private final String name;
         private final String passwordDigest;
         private final String salt;
-        private final UserRole role;
 
-        public ServerUser(int id, String name, String passwordDigest, String salt, UserRole role) {
+
+        public ServerUser(int id, String name, String passwordDigest, String salt) {
             this.id = id;
             this.name = name;
             this.passwordDigest = passwordDigest;
             this.salt = salt;
-            this.role = role;
         }
 
         public int getId() {
             return id;
         }
-
-        public String getName() {
-            return name;
-        }
-
         public String getPasswordDigest() {
             return passwordDigest;
         }
@@ -33,8 +25,5 @@ public class ServerUser {
             return salt;
         }
 
-        public UserRole getRole() {
-            return role;
-        }
     }
 
