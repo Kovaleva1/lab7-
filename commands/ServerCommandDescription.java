6fa90ce705206221cdabeb3af61974b52f584809
Kovaleva1 <115421637+Kovaleva1@users.noreply.github.com>
2023-06-11 01:09:54 +0300

    Delete ServerCommandDescription.java

--- commands/ServerCommandDescription.java
+++
@@ -1,6 +0,0 @@
-package commands;
-public class ServerCommandDescription extends CommandDescription{
-    public ServerCommandDescription(AbstractCommand command) {
-        super(command);
-    }
-}
